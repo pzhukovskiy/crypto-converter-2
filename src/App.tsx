5767379e248f3fdb3ad86f0c55ea2283e29fd567
Pavel Zhukovskiy <pavel20122004@mail.ru>
2023-10-13 22:53:59 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import './App.css'
 import HomePage from './pages/HomePage'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ListAbout from './components/about/ListAbout'
-import NotFound from './components/notFound/NotFound'
 
 const App = () => {
 
   return (
-    <BrowserRouter>
-    <Routes>
-       <Route path='/' element={<HomePage/>}/>
-       <Route path="/about/:rank" element={<ListAbout/>}/>
-       <Route path="*" element={<NotFound/>}/>
-     </Routes>
- </BrowserRouter>
+    <div>
+      <HomePage/>
+    </div>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ListAbout from './components/about/ListAbout';
 import NotFound from './components/notFound/NotFound';
 
@@ -14,8 +14,12 @@ const queryClient = new QueryClient();
 
 root.render(
   <QueryClientProvider client={queryClient}>
-    <HashRouter>
-      <App/>
-    </HashRouter>
+    <BrowserRouter>
+       <Routes>
+          <Route path='/' element={<App/>}/>
+          <Route path="/about/:rank" element={<ListAbout/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+    </BrowserRouter>
   </QueryClientProvider>
 );
